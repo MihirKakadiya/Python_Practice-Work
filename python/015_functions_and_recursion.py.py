95532c6ffa572891945873835dc1397d56a2f4bb
Mihir Kakadiya <165221404+MihirKakadiya@users.noreply.github.com>
2024-03-28 19:52:39 -0400

    Update 015_functions_and_recursion.py.py

--- python/015_functions_and_recursion.py.py
+++ python/015_functions_and_recursion.py.py
@@ -15,141 +15,141 @@ print(avg1,avg)
 
 # ...................................................
 
-# def greet(name):
-#     print("Good day," + name)
+def greet(name):
+    print("Good day," + name)
 
-# greet("mihir")#function calling............
+greet("mihir")#function calling............
 
 # .........................................................
 
-# def mysum(n1,n2):
-#     return n1+n2+1
+def mysum(n1,n2):
+    return n1+n2+1
 
-# S = mysum(2,3)
-# print(S)
+S = mysum(2,3)
+print(S)
 
-# def greet(name="stranger"):   #defult parameter.............
-#     print("Hellow," + name)
+def greet(name="stranger"):   #defult parameter.............
+    print("Hellow," + name)
 
-# greet("mihir")
-# greet()  #<------------------------------------
+greet("mihir")
+greet()  #<------------------------------------
 
 #recursion.................................
 
-# n = 5
-# p=1
+n = 5
+p=1
 
-# for i in range(n):
-#     p = p*(i+1)
-# print(p)
+for i in range(n):
+    p = p*(i+1)
+print(p)
 
-# def fect(n):
-#     p=1
+def fect(n):
+    p=1
     
-#     for i in range(n):
-#         p = p*(i+1)
-#     return p
+    for i in range(n):
+        p = p*(i+1)
+    return p
 
-# f = fect(5)
-# print(f)
+f = fect(5)
+print(f)
 
 #recursion....................
 
-# def fectrec(n):
-#     if n == 1 or n ==0:
-#         return 1
-#     return n * fectrec(n-1)
+def fectrec(n):
+    if n == 1 or n ==0:
+        return 1
+    return n * fectrec(n-1)
 
-# f = fectrec(0)
-# print(f)
+f = fectrec(0)
+print(f)
 
 
 #1..............................
 
-# n1 = int(input("Enter a number :"))
-# n2 = int(input("Enter a number :"))
-# n3 = int(input("Enter a number :"))
+n1 = int(input("Enter a number :"))
+n2 = int(input("Enter a number :"))
+n3 = int(input("Enter a number :"))
 
-# def biggest(n1,n2,n3):
-#     if n1>n2 :
+def biggest(n1,n2,n3):
+    if n1>n2 :
         
-#         if(n1>n3):
-#             return n1
-#         else:
-#             return n3
+        if(n1>n3):
+            return n1
+        else:
+            return n3
         
-#     else:
-#         if(n2>n3):
-#             return n2
-#         else:
-#             return n3
+    else:
+        if(n2>n3):
+            return n2
+        else:
+            return n3
 
-# m = biggest(n1,n2,n3)
-# print(m)
+m = biggest(n1,n2,n3)
+print(m)
 
 #2.................................
 
-# n = int(input("Enter a temp :"))
+n = int(input("Enter a temp :"))
 
-# def temp(n):
-#     return (n * (9/5) )+ 32
-# c = temp(n)
-# print(c)
+def temp(n):
+    return (n * (9/5) )+ 32
+c = temp(n)
+print(c)
         
 #3..................................
 
-# n = int(input("Enter a number :"))
+n = int(input("Enter a number :"))
 
 
-# def number(n):
-#     if  n == 0:
-#         return 0
-#     return n + number(n-1)
+def number(n):
+    if  n == 0:
+        return 0
+    return n + number(n-1)
     
-# a = number(n)
-# print(a)
+a = number(n)
+print(a)
 
 #4..............
-# n = int(input("Enter a number :"))
+n = int(input("Enter a number :"))
 
-# def pettern(n):
-#     for i in range(n):
-#          print("*" * (n-i))
+def pettern(n):
+    for i in range(n):
+         print("*" * (n-i))
     
-# a = pettern(n)
-# print(a)
+a = pettern(n)
+print(a)
 
 #5.........................
     
 
-# n = int(input("Enter a number :"))
+n = int(input("Enter a number :"))
 
-# def cm(n):
-#     return n * 2.54
+def cm(n):
+    return n * 2.54
 
-# inch = cm(n)
-# print(inch)
+inch = cm(n)
+print(inch)
 
 #6....................................
 
-# s = "    hello mihir you are great   "
-# def remove(s,b):
-#     nstr= s.replace(b,"")
-#     return nstr.strip()
-# s = remove(s,"hello")
-# print(s)
+s = "    hello mihir you are great   "
+def remove(s,b):
+    nstr= s.replace(b,"")
+    return nstr.strip()
+s = remove(s,"hello")
+print(s)
 
 #7.....................................
 
-# n = int(input("Enter a number :"))
+n = int(input("Enter a number :"))
 
-# def multiplication(n):
-#     i=1
-#     while i<=10:
-#         print(n*i)
-#         i = i+1
+def multiplication(n):
+    i=1
+    while i<=10:
+        print(n*i)
+        i = i+1
 
-# multiplication(n)
+multiplication(n)
 
 
 
