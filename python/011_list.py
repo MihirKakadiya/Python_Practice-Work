0b815795a7694fa076335015fcc88e1d9974b651
Mihir Kakadiya <165221404+MihirKakadiya@users.noreply.github.com>
2024-03-28 19:26:16 -0400

    Update 011_list.py

--- python/011_list.py
+++ python/011_list.py
@@ -1,30 +1,28 @@
 #list..................................
 
-# a = [1,2,3,4,56]
-# print(a)
+a = [1,2,3,4,56]
+print(a)
+print(a[-1])
 
-# print(a[-1])
+a[-2]  = 24
+print(a)
 
-# a[-2]  = 24
-# print(a)
-
-# c= ["SPike","mihir","HELLO","World","DAE","PYTHON","BYE"]
-# print(c[0::3])
+c= ["SPike","mihir","HELLO","World","DAE","PYTHON","BYE"]
+print(c[0::3])
 
 #methods...........................
-# d = [1,11,9,4,5,6,7]
-# d.sort()
-# d.reverse()
-# d.append(50)
-# d.insert(2,3)
-# d.pop(2)
-# d.remove(7)
-# print(d)
+d = [1,11,9,4,5,6,7]
+d.sort()
+d.reverse()
+d.append(50)
+d.insert(2,3)
+d.pop(2)
+d.remove(7)
+print(d)
 
 #...................TUPLE.................
 t = (1,2,3,2,5,2,7)
 print(t)
-
 T=(1,)
 print(T)
 
@@ -34,42 +32,42 @@ print(t.count(2))
 print(t.index(5))
 
 #3........
-# t[0]=2
-# print(t)
+t[0]=2
+print(t)
 
 #1..............
 
-# f = input("Enter fruits name : ")
-# f1 = input("Enter fruits name : ")
-# f2 = input("Enter fruits name : ")
-# f3 = input("Enter fruits name : ")
-# f4 = input("Enter fruits name : ")
-# f5 = input("Enter fruits name : ")
-# f6 = input("Enter fruits name : ")
-# fruits = [f,f1,f2,f,f4,f5,f6]
-# print(fruits)
+f = input("Enter fruits name : ")
+f1 = input("Enter fruits name : ")
+f2 = input("Enter fruits name : ")
+f3 = input("Enter fruits name : ")
+f4 = input("Enter fruits name : ")
+f5 = input("Enter fruits name : ")
+f6 = input("Enter fruits name : ")
+fruits = [f,f1,f2,f,f4,f5,f6]
+print(fruits)
 
 #2....................
 
-# s1 = int(input("Enter a marks : "))
-# s2 = int(input("Enter a marks : "))
-# s3 = int(input("Enter a marks : "))
-# s4 = int(input("Enter a marks : "))
-# s5 = int(input("Enter a marks : "))
-# s6 = int(input("Enter a marks : "))
+s1 = int(input("Enter a marks : "))
+s2 = int(input("Enter a marks : "))
+s3 = int(input("Enter a marks : "))
+s4 = int(input("Enter a marks : "))
+s5 = int(input("Enter a marks : "))
+s6 = int(input("Enter a marks : "))
 
-# Student = [s1,s2,s3,s4,s5,s6]
-# Student.sort()
-# print(Student)
+Student = [s1,s2,s3,s4,s5,s6]
+Student.sort()
+print(Student)
 
 #4.........
 
-# a= [2,4,2,6,5,6]
-# print(a[0] + a[1] + a[2] + a[3])
-# print(sum(a))
+a= [2,4,2,6,5,6]
+print(a[0] + a[1] + a[2] + a[3])
+print(sum(a))
 
 #5.............
 
-# z= (0,5,3,0,5,0,1,2,0)
-# print(z.count(0))
+z= (0,5,3,0,5,0,1,2,0)
+print(z.count(0))
 
