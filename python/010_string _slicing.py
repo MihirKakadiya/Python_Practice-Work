d04e442d748d7729960ede148724aa9e2c23d3d4
Mihir Kakadiya <165221404+MihirKakadiya@users.noreply.github.com>
2024-03-28 19:12:55 -0400

    Update 010_string _slicing.py

--- python/010_string _slicing.py
+++ python/010_string _slicing.py
@@ -1,55 +1,56 @@
-# a = "hello Spike "
-# b = "World"
-# c = a+b
-# print(c)
-# print(a[1:4])
-# print(b[-4:-1])
-# #slicing with skip values
-# print(a[0::3])
+ a = "hello Spike "
+ b = "World"
+ c = a+b
+ print(c)
+ print(a[1:4])
+ print(b[-4:-1])
 
-# #functions
-# c = "once a time for spike"
-# print(c[0:5])
-# print(len(c))
-# print(c.endswith("spike"))
-# print(c.count("spike"))
-# print(c.capitalize())
-# print(c.find("o"))
-# print(c.replace("spike","mihir"))
+# slicing with skip values
+ print(a[0::3])
 
-# e = "hi mihir.\nI am world"
-# print(e)
+# functions
+c = "once a time for spike"
+print(c[0:5])
+print(len(c))
+print(c.endswith("spike"))
+print(c.count("spike"))
+print(c.capitalize())
+print(c.find("o"))
+print(c.replace("spike","mihir"))
+
+e = "hi mihir.\nI am world"
+print(e)
 
 #1
-# name = input("Enter u r name : ")
-# print("GOOD AFTERNOON : ",name)
+name = input("Enter u r name : ")
+print("GOOD AFTERNOON : ",name)
 
 
 #2
-# letter = '''Dear <|name|>,
-# You are selected!
-# congrats!!!
-# Date: <|DATE|>'''
+letter = '''Dear <|name|>,
+          You are selected!
+          congrats!!!
+          Date: <|DATE|>'''
 
-# name = input("Enter u r name\n")
-# Date = input("Enter a date")
+name = input("Enter u r name\n")
+Date = input("Enter a date")
 
-# letter = letter.replace("<|name|>", name)
-# letter = letter.replace("<|DATE|>", Date)
+letter = letter.replace("<|name|>", name)
+letter = letter.replace("<|DATE|>", Date)
 
-# print(letter)
+print(letter)
 
 #3
 
-# m = "hi  hello mihir  spike"
-# doublespace = m.find("  ")
-# print(doublespace)
+m = "hi  hello mihir  spike"
+doublespace = m.find("  ")
+print(doublespace)
 
 #4
 
-# m = "hi  hello mihir  spike"
-# doublespace = m.replace("  "," ")
-# print(doublespace)
+m = "hi  hello mihir  spike"
+doublespace = m.replace("  "," ")
+print(doublespace)
 
 #5
 l = "Dear mihir,\nyou are very nice student!!,\n\t\tthanks"
