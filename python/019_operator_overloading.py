3ac554568f7ad1d4ec44da0102425579aa17b8c6
Mihir Kakadiya <165221404+MihirKakadiya@users.noreply.github.com>
2024-03-30 01:34:31 -0400

    Update 019_operator_overloading.py

--- python/019_operator_overloading.py
+++ python/019_operator_overloading.py
@@ -1,20 +1,20 @@
 
-# class number:
-#     def __init__(self,num):
-#         self.num = num
+class number:
+    def __init__(self,num):
+        self.num = num
 
-#     def __add__(self,num2):
-#         print("ADD")
-#         return self.num + num2.num
+    def __add__(self,num2):
+        print("ADD")
+        return self.num + num2.num
 
-#     def __mul__(self,num2):
-#         print("ADD")
-#         return self.num * num2.num
+    def __mul__(self,num2):
+        print("ADD")
+        return self.num * num2.num
 
-# n1 = number(4)
-# n2= number(5)
+n1 = number(4)
+n2= number(5)
 
-# sum = n1 + n2
-# mul = n1*n2
-# print(sum)
+sum = n1 + n2
+mul = n1*n2
+print(sum)
 # print(mul)
