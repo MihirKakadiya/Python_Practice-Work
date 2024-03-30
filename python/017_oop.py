76085b8e55c58aaba26c9403974183b434bf2a91
Mihir Kakadiya <165221404+MihirKakadiya@users.noreply.github.com>
2024-03-30 01:29:40 -0400

    Update 017_oop.py

--- python/017_oop.py
+++ python/017_oop.py
@@ -1,156 +1,153 @@
-# class number:
-#     def sum(self):
-#         return self.a + self.b
+class number:
+    def sum(self):
+        return self.a + self.b
 
-# num = number()
+num = number()
 
-# num.a =12
-# num.b =34
-# s = num.sum()
-# print(s)
+num.a =12
+num.b =34
+s = num.sum()
+print(s)
 
-# a =12
-# b =34
+a =12
+b =34
 
-# print("sum of a nd b is", a+b)
+print("sum of a nd b is", a+b)
 
-# class Employee:
-#     company= "Tesla"
-#     salary= 500000 #class attribute
+class Employee:
+    company= "Tesla"
+    salary= 500000 #class attribute
 
-# spike = Employee()
-# mihir = Employee()
+spike = Employee()
+mihir = Employee()
 
-# spike.salary = 600000 #instance attribute
-# print(spike.salary)
-# print(mihir.salary) #class attribute
+spike.salary = 600000 #instance attribute
+print(spike.salary)
+print(mihir.salary) #class attribute
 
 #....................self.......................
 
-# class Employee:
-#     company= "Tesla"
-#     def getsalary(self):
-#         print(f"Salary of this employee is {self.salary} in comapany {self.company}")
+class Employee:
+    company= "Tesla"
+    def getsalary(self):
+        print(f"Salary of this employee is {self.salary} in comapany {self.company}")
 
-# spike = Employee()
+spike = Employee()
 
 
-# spike.salary = 600000 
-# spike.getsalary()
+spike.salary = 600000 
+spike.getsalary()
 
 #......................static................
 
-# class Employee:
-#     company= "Tesla"
-#     def getsalary(self):
-#         print(f"Salary of this employee is {self.salary} in comapany {self.company}")
+class Employee:
+    company= "Tesla"
+    def getsalary(self):
+        print(f"Salary of this employee is {self.salary} in comapany {self.company}")
     
-#     @staticmethod
-#     def greet():
-#         print("Hello sir, Good morning")
+    @staticmethod
+    def greet():
+        print("Hello sir, Good morning")
 
-# spike = Employee()
+spike = Employee()
 
 
-# spike.salary = 600000 
-# spike.getsalary()
-# spike.greet()
+spike.salary = 600000 
+spike.getsalary()
+spike.greet()
 
 #.................constructor....................
 
-# class Employee:
-#     company= "Tesla"
+class Employee:
+    company= "Tesla"
 
-#     def __init__(self,name,salary,subunit):
-#         self.name = name
-#         self.salary= salary
-#         self.subunit = subunit
-#         print("Employee is created")
+    def __init__(self,name,salary,subunit):
+        self.name = name
+        self.salary= salary
+        self.subunit = subunit
+        print("Employee is created")
     
-#     def getdetails(self):
-#         print(f"the name is {self.name}")
-#         print(f"the salary is {self.salary}")
-#         print(f"the company is {self.subunit}")
+    def getdetails(self):
+        print(f"the name is {self.name}")
+        print(f"the salary is {self.salary}")
+        print(f"the company is {self.subunit}")
         
-#     def getsalary(self):
-#         print(f"Salary of this employee is {self.salary} in comapany {self.company}")
+    def getsalary(self):
+        print(f"Salary of this employee is {self.salary} in comapany {self.company}")
     
-#     @staticmethod
-#     def greet():
-#         print("Hello sir, Good morning")
+    @staticmethod
+    def greet():
+        print("Hello sir, Good morning")
 
-# spike = Employee("Spike",100,"Google")
-
-
-
-# spike.getsalary()
-# spike.greet()
-# spike.getdetails()
+spike = Employee("Spike",100,"Google")
+spike.getsalary()
+spike.greet()
+spike.getdetails()
 
 #1..................................
 
-# class Programmer:
+class Programmer:
 
-#     company = "Google"
+    company = "Google"
 
-#     def __init__(self,name,product):
-#         self.name = name
-#         self.product= product
+    def __init__(self,name,product):
+        self.name = name
+        self.product= product
 
-#     def getinfo(self):
-#         print(f"comanap is {self.company} and programmer name is {self.name} and working on {self.product} ")
+    def getinfo(self):
+        print(f"comanap is {self.company} and programmer name is {self.name} and working on {self.product} ")
 
 
-# spike = Programmer("Mihir","Map")
-# MIhir = Programmer("ZEEL","YT")
-# spike.getinfo()
-# MIhir.getinfo()
+spike = Programmer("Mihir","Map")
+MIhir = Programmer("ZEEL","YT")
+spike.getinfo()
+MIhir.getinfo()
 
 #2......................................
 
-# a = int(input("Enter a number : "))
+a = int(input("Enter a number : "))
 
-# class Calculator:
-#     def __init__(self,num):
-#         self.number = num
+class Calculator:
+    def __init__(self,num):
+        self.number = num
 
-#     def square(self):
-#         print(f"Squre is {self.number * self.number}")
-#     def cube(self):
-#         print(f"cube is {self.number **3}")
-#     def root(self):
-#         print(f"Squreroot is {self.number **0.5}")
+    def square(self):
+        print(f"Squre is {self.number * self.number}")
+    def cube(self):
+        print(f"cube is {self.number **3}")
+    def root(self):
+        print(f"Squreroot is {self.number **0.5}")
 
-# b = Calculator(a) 
-# b.square() 
-# b.cube()
-# b.root()  
+b = Calculator(a) 
+b.square() 
+b.cube()
+b.root()  
 
 
 #4..................................
 
-# a = int(input("Enter a number : "))
+a = int(input("Enter a number : "))
 
-# class Calculator:
-#     def __init__(self,num):
-#         self.number = num
+class Calculator:
+    def __init__(self,num):
+        self.number = num
 
-#     def square(self):
-#         print(f"Squre is {self.number * self.number}")
-#     def cube(self):
-#         print(f"cube is {self.number **3}")
-#     def root(self):
-#         print(f"Squreroot is {self.number **0.5}")
+    def square(self):
+        print(f"Squre is {self.number * self.number}")
+    def cube(self):
+        print(f"cube is {self.number **3}")
+    def root(self):
+        print(f"Squreroot is {self.number **0.5}")
 
-#     @staticmethod
-#     def greet():
-#         print("Good Morning Folks !!!")
+    @staticmethod
+    def greet():
+        print("Good Morning Folks !!!")
 
-# b = Calculator(a) 
-# b.greet()
-# b.square() 
-# b.cube()
-# b.root() 
+b = Calculator(a) 
+b.greet()
+b.square() 
+b.cube()
+b.root() 
 
 
 #5..........................
